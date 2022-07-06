c491ce1f5617bcf8c4474fc4a95d0a8c6e5fbb73
Ruihang Xia <waynestxia@gmail.com>
2022-07-06 18:16:39 +0800

    feat: display rows in csv format
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

---
+++ src/model/display.rs
@@ -0,0 +1,26 @@
+use std::fmt::Display;
+
+use crate::model::QueriedRows;
+
+/// Display [QueriedRows] in csv format.
+pub struct CsvFormatter {
+    pub rows: QueriedRows,
+}
+
+impl Display for CsvFormatter {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for column in &self.rows.schema.column_schemas {
+            f.write_fmt(format_args!("{},", column.name))?;
+        }
+        f.write_str(",\n")?;
+
+        for row in &self.rows.rows {
+            for datum in &row.datums {
+                f.write_fmt(format_args!("{:?},", datum))?;
+            }
+            f.write_str(",\n")?;
+        }
+
+        Ok(())
+    }
+}
