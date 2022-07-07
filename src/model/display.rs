cab40e17349bd02fc8f2fb2ffc014b1ea1005189
Ruihang Xia <waynestxia@gmail.com>
2022-07-07 11:27:21 +0800

    remove tailing double commas
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- src/model/display.rs
+++ src/model/display.rs
@@ -12,13 +12,13 @@ impl Display for CsvFormatter {
         for column in &self.rows.schema.column_schemas {
             f.write_fmt(format_args!("{},", column.name))?;
         }
-        f.write_str(",\n")?;
+        f.write_str("\n")?;
 
         for row in &self.rows.rows {
             for datum in &row.datums {
                 f.write_fmt(format_args!("{:?},", datum))?;
             }
-            f.write_str(",\n")?;
+            f.write_str("\n")?;
         }
 
         Ok(())
