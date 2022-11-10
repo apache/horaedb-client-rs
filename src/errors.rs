9ca236352bd28eb6ebc8f65ffdbc8a4db5c86bb9
Arthur Chern <weirong.cwr@gmail.com>
2022-11-10 21:01:43 +0800

    refactor: remove unused import

--- src/errors.rs
+++ src/errors.rs
@@ -1,6 +1,6 @@
 // Copyright 2022 CeresDB Project Authors. Licensed under Apache-2.0.
 
-use crate::{model::write::WriteResponse, RpcContext};
+use crate::{model::write::WriteResponse};
 
 #[derive(Debug)]
 pub enum Error {
