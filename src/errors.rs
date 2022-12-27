fb9622a201039735fe6ab343ca4b6562117fe54e
kamille <34352236+Rachelint@users.noreply.github.com>
2022-12-27 19:19:19 +0800

    feat: standardize error (#26)
    * standardize error by `thiserror`.
    
    * adgress CR.

--- src/errors.rs
+++ src/errors.rs
@@ -1,20 +1,27 @@
 // Copyright 2022 CeresDB Project Authors. Licensed under Apache-2.0.
 
+use std::fmt::Display;
+
+use thiserror::Error as ThisError;
+
 use crate::model::write::WriteResponse;
 
-#[derive(Debug)]
+#[derive(Debug, ThisError)]
 pub enum Error {
-    /// Error from the running server.
+    /// Error from the running server
+    #[error("failed in server, err:{0}")]
     Server(ServerError),
 
-    /// Error from the rpc.
+    /// Error from the rpc
     /// Note that any error caused by a running server wont be wrapped in the
     /// grpc errors.
+    #[error("failed in grpc, err:{0}")]
     Rpc(tonic::Status),
 
     /// Error about rpc.
     /// It will be throw while connection between client and server is broken
     /// and try for reconnecting is failed(timeout).
+    #[error("failed to connect, addr:{addr:?}, err:{source:?}")]
     Connect {
         addr: String,
         source: Box<dyn std::error::Error + Send + Sync>,
@@ -22,15 +29,20 @@ pub enum Error {
 
     /// Error from the client and basically the rpc request has not been called
     /// yet or the rpc request has already been finished successfully.
+    #[error("failed in client, msg:{0}")]
     Client(String),
 
     /// Error about authentication
+    #[error("failed to check auth, err:{0}")]
     AuthFail(AuthFailStatus),
 
-    ///
+    /// Error from write in cluster mode, some of rows may be written
+    /// successfully, and others may fail.
+    #[error("failed in cluster write, err:{0}")]
     ClusterWriteError(ClusterWriteError),
 
     /// Error unknown
+    #[error("unknown error, msg:{0}")]
     Unknown(String),
 }
 
@@ -72,10 +84,28 @@ impl ClusterWriteError {
     }
 }
 
+impl Display for ClusterWriteError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("ClusterWriteError")
+            .field("ok", &self.ok)
+            .field("errors", &self.errors)
+            .finish()
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct ServerError {
     pub code: u32,
     pub msg: String,
+}
+
+impl Display for ServerError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("ServerError")
+            .field("code", &self.code)
+            .field("msg", &self.msg)
+            .finish()
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -93,4 +123,31 @@ pub enum AuthCode {
     InvalidTokenMeta = 2,
 }
 
+impl Display for AuthFailStatus {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("AuthFailStatus")
+            .field("code", &self.code)
+            .field("msg", &self.msg)
+            .finish()
+    }
+}
+
 pub type Result<T> = std::result::Result<T, Error>;
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_error_standardizing() {
+        let source_error = Box::new(Error::Unknown("unknown error".to_string()));
+        let connect_error = Error::Connect {
+            addr: "1.1.1.1:1111".to_string(),
+            source: source_error as _,
+        };
+        assert_eq!(
+            &format!("{}", connect_error),
+            r#"failed to connect, addr:"1.1.1.1:1111", err:Unknown("unknown error")"#
+        );
+    }
+}
